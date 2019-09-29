51221bf85160e2c5e79b3525dc7e36975fa8942a
jtwining <jtwining@cc931e52-e693-435a-9507-8c748cdfb6f4>
2019-09-29 18:17:24 +0000

    make multiply.cpp

---
+++ bigint/multiply.cpp
@@ -0,0 +1,35 @@
+// Project 1 Milestone 3
+// Justin Twining
+//
+//
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "bigint.hpp"
+
+int main() {
+  std::ifstream in("data1-.txt");    // Define stream for input
+  if(!in) {                           // Make sure it opened correctly.
+    std::cerr << "Could not open data1-1.txt, exiting." << std::endl;
+    exit(1);
+  }
+    
+  bigint (lhs);
+  bigint (rhs);
+  bigint (result);
+
+  while(!in.eof() && in >> lhs){
+    if (!in.eof()) in >> rhs;
+    //prints bigints
+    std::cout << "lhs = " << lhs << std::endl << std::endl
+	      << "rhs = " << rhs << std::endl << std::endl;
+
+    //multiplies bigints and print result
+    result = lhs * rhs;
+    std::cout << "result = " << result << std::endl << std::endl;
+   
+  }
+ 
+  in.close();
+  return 0;
+}
