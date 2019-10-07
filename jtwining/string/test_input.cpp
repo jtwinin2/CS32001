7ed87c082b7e6d3d1e7d12f10768114d685ebc20
jtwining <jtwining@cc931e52-e693-435a-9507-8c748cdfb6f4>
2019-10-07 04:27:40 +0000

    Testing input finished

--- jtwining/string/test_input.cpp
+++ jtwining/string/test_input.cpp
@@ -7,10 +7,30 @@
 #include "string.hpp"
 #include <cassert>
 #include <iostream>
-
+#include <fstream>
 //===========================================================================
 int main ()
 {
+  std::ifstream file("data_input.txt");
+  if (file.is_open()) {
+    String x;
+    file >> x;
+    assert(x == "hello");
+    file >> x;
+    assert(x == "world");
+    file >> x;
+    assert(x == "my");
+    file >> x;
+    assert(x == "name"); 
+    file >> x;
+    assert(x == "is"); 
+    file >> x;
+    assert(x == "Justin");
+  }
+  else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+  file.close();
 
   std::cout << "Done testing Input." << std::endl;
 
