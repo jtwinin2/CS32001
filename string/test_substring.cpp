24a4fb2fe0f00cf31274b3bd3a754e580b933e02
jtwining <jtwining@cc931e52-e693-435a-9507-8c748cdfb6f4>
2019-10-20 18:39:52 +0000

    substring passes all tests

--- string/test_substring.cpp
+++ string/test_substring.cpp
@@ -36,9 +36,9 @@ int main ()
     assert (str.substr( 8 , 12) == "wgegl" );
   }
   {
-    String str = "S2CTS9yw7YiYeRfsUXUqsLWd1TZUlTTs9xMhvDuZy1cf9YwTdm";
+    String str = "SCTSywYiYeRfsUXUqsLWd1TZUlTTs9xMhvDuZy1cf9YwTdm";
 
-    assert (str.substr( 0 , 20) == "S2CTS9yw7YiYeRfsUXUqs" );
+    assert (str.substr( 0 , 7) == "SCTSywYi" );
   }
   {
     String str = "yPB4Tn9NG9kKODTKQCv4EXY1YNpTnY2SWck4A8CNvoviRHvCCnMsWjou61s3t9y9kC8u9pW8zsA";
@@ -46,9 +46,9 @@ int main ()
     assert (str.substr( 49 , 58 ) == "nMsWjou61s" );
   }
   {
-    String str = "o1C9njpUtoHcd1n0RbdQHCSoppwgCGNhMY6HfqnHmnMK9nV7hHPDBLQWTMMRo3txu3oOl7VrzUNthio7sVnWp1ZvLfk7eblXVB67dsMd8LH2qobc267NxTK3xtGVgPouLtDBNDxWmjQiDBWyKDtCuS";
+    String str = "CnjpUtoHcdnRbdQHCSoppwgCGNhMYHfqnHmnMKnVhHPDBLQWTMMRotxuoOl7VrzUNthio7sVnWp1ZvLfk7eblXVB67dsMd8LH2qobc267NxTK3xtGVgPouLtDBNDxWmjQiDBWyKDtCuS";
 
-    assert (str.substr( 2 , 149 ) == "C9njpUtoHcd1nnRbdQHCSoppwgCGNhMY6HfqnHmnMK9nV7hHPDBLQWTMMRo3txu3oOl7VrzUNthio7sVnWp1ZvLfk7eblXVB67dsMd8LH2qobc267NxTK3xtGVgPouLtDBNDxWmjQiDBWyKDtCuS" );
+    assert (str.substr( 41 , 47 ) == "HPDBLQW" );
   }
   {
     String str = "wKttpwIrDHyV8aEm9bSra1j2JwxFWzevHZl6kXSTmNaljn5Ct1g2sEgTBSOwfDDGIcWT1cQW8tmkaKcz1Xfdpz6vBcfXyAFfzovJNPjRQcYwGv4OMSOM6j6HW9ulnRLfd83jP6g2Y4K6J2OgnbeWvB5oBEdMPoXdD7CIKWVVGN98nBL2AjnnvwztjqvzpHY4mz3LCSCn";
